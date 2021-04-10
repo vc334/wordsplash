9c090eb28375d537f5c92eb390a1f196e69e8935
Victor Chiang <vgchiang@gmail.com>
2021-04-10 13:34:19 -0700

    commented out the test action in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,10 @@ class PagesController < ApplicationController
   def home
   end
 
-  def test
-
-    translator = BingTranslator.new('cc3610100b0b44ad94f7a3dc9832a6c8')
-
-    # Translation
-
-    @spanish = translator.translate('Hello. This will be translated!', :from => 'en', :to => 'es')
-    # spanish = translator.translate('Hello. This will be translated!', :to => 'es')
-
-  end
+  # def test
+  #   translator = BingTranslator.new('###')
+  #   # Translation
+  #   @spanish = translator.translate('Hello. This will be translated!', :from => 'en', :to => 'es')
+  #   # spanish = translator.translate('Hello. This will be translated!', :to => 'es')
+  # end
 end
